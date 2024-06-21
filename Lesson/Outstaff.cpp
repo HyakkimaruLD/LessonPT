161a456808ce70b23d83459d304e14743c233cb5
Yaroslav <yaroslav@keyoneinc.uk>
2024-06-21 19:25:13 +0100

    added class Outstaff, and it's defenition

--- Lesson/Outstaff.cpp
+++ Lesson/Outstaff.cpp
@@ -1,1 +1,10 @@
 #include "Outstaff.h"
+
+Outstaff::Outstaff(string name, string position, double salary) : Employee(name, position, salary)
+{
+}
+
+double Outstaff::calcSalary()
+{
+	return salary;
+}
--- Lesson/Outstaff.h
+++ Lesson/Outstaff.h
@@ -1,7 +1,12 @@
 #pragma once
-class Outstaff
+#include "Employee.h"
+
+class Outstaff : public Employee
 {
 
-	
+public:
+	Outstaff(string name, string position, double salary);
+
+	double calcSalary();
 };
 
