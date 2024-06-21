c53f6e3aeb023798ad4838a570441bba5e6cff8f
Vlad Chernukha <forreadtr@gmail.com>
2024-06-21 19:41:36 +0300

    class Employee

---
+++ Lesson/Employee.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Employee
+{
+protected:
+	string name;
+	string positiong;
+	float salary;
+public:
+	Employee();
+	Employee(string name, string positiong, float salary);
+	virtual ~Employee();
+
+	void setName(string name);
+	void setPosition(string position);
+	void setSalary(float salary);
+
+	string getName()const;
+	string getPosition()const;
+	float getSalary()const;
+
+	virtual void showInfo()const = 0;
+	virtual float calcSalary()const = 0;
+};
